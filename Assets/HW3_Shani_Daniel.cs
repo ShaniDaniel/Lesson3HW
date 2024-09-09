521c370b7163444ef1f9004b0b464b5a94b5b7a7
Lydia Dover <68915121+doverlydia@users.noreply.github.com>
2024-09-09 21:14:18 +0300

    Lydias notes
    Lydias notes

--- Assets/HW3_Shani_Daniel.cs
+++ Assets/HW3_Shani_Daniel.cs
@@ -7,6 +7,7 @@ public class HW3_Shani_Daniel : MonoBehaviour
 
     void Start()
     {
+    // bad naming. this doesnt tell me what the method do
         method1(25);
         method2(2);
         method2(7);
@@ -15,12 +16,13 @@ public class HW3_Shani_Daniel : MonoBehaviour
 
     }
 
+// shold call it DivideByHaldAddSeven();
     public static void method1(int number)
     {
         int result = number / 2 + 7;
         Debug.Log(result);
     }
-
+// shold call it IsBiggerThanFive();
     public static void method2(int number)
     {
         if (number < 5){
@@ -31,7 +33,7 @@ public class HW3_Shani_Daniel : MonoBehaviour
             Debug.Log($"The number {number} is bigger than 5");
         }
     }
-
+// shold call it ValidateNumber();
     public static void method3(int number)
     {
         switch (number)
